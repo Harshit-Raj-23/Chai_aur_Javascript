ea9e530429b2e8707351fa5b58808c8ea2b62f62
Harshit-Raj-23 <154404091+Harshit-Raj-23@users.noreply.github.com>
2024-01-27 08:28:26 +0000

    datatypes done

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS codes as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3)   // code readability should be high
+
+console.log("Harshit")
+
+let name = "harshit"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "harshit")
+console.log(typeof null)
+console.log(typeof undefined)
