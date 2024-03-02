29de4aca8a9e28c37bcdda2b04b2620e52e8de02
Harshit-Raj-23 <154404091+Harshit-Raj-23@users.noreply.github.com>
2024-03-02 20:41:46 +0000

    Stack & Heap

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -13,14 +13,14 @@ let num = 6523487243984896892349864986984132689n
 let id = Symbol("123")
 let anotherId = Symbol("123")
 
-console.log(typeof score);
-console.log(typeof name);
-console.log(typeof isLoggedIn);
-console.log(typeof temp);
-console.log(typeof state);
-console.log(typeof num);
-console.log(typeof id);
-console.log(id === anotherId);
+// console.log(typeof score);
+// console.log(typeof name);
+// console.log(typeof isLoggedIn);
+// console.log(typeof temp);
+// console.log(typeof state);
+// console.log(typeof num);
+// console.log(typeof id);
+// console.log(id === anotherId);
 
 // Reference (non-primitive)
 
@@ -28,12 +28,41 @@ console.log(id === anotherId);
 
 let heroes = ["shaktiman", "naagraj", "doga"]
 let myObject = {
-    name: "Harshit"
+    name: "Harshit",
+    age: 19
 }
 let myFunction = function(){
     console.log("Hello Harshit");
 }
 
-console.log(typeof heroes);
-console.log(typeof myFunction);
-console.log(typeof myObject);
+// console.log(typeof heroes);
+// console.log(typeof myFunction);
+// console.log(typeof myObject);
+
+// ****************************************************************************************
+
+// Stack (Primitive)   and    Heap (Non primitive)
+
+// Stack:
+
+let username = "Harshit"
+console.log(username);
+let anotherUsername = username
+console.log(anotherUsername);
+anotherUsername = "HarshitRaj"
+console.log(anotherUsername);
+
+// Heap:
+
+let user1 = {
+    email: "user@gmail.com",
+    password: 123456
+}
+console.log(user1);
+
+let user2 = user1
+console.log(user2);
+
+user1.email = "harshit@gmail.com"
+console.log(user1.email);
+console.log(user2.email);
